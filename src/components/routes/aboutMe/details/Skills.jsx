5dc58d2cd7eeabb05ada46cbb6d0c6dfcf0539fb
jesuschock93 <jesuschock93@gmail.com>
2023-11-29 11:26:34 -0400

    fixed2

--- src/components/routes/aboutMe/details/Skills.jsx
+++ src/components/routes/aboutMe/details/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import ImgCode from '../../../Utils/ImgCode'
+import ImgCode from './components/utils/ImgCode.jsx'
 
 
 const SkillsAlls = () => {
